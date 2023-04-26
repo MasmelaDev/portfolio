74a2a0955f6057e85157864f43127e3e92c714c5
MasmelaDev <miguelmasmeladev@gmail.com>
2023-04-26 10:46:06 -0500

    bg gradient light added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router";
-import "./App.css";
 import { Home } from "./pages/Home";
 import { Header } from "./components/Header";
 import { InfoCard } from "./components/InfoCard";
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ export default {
   theme: {
     extend: {
       backgroundImage:{
-        "darkGradient": "radial-gradient(circle, rgba(13,6,73,1) 0%, rgba(85,42,214,1) 100%)"
+        "darkGradient": "radial-gradient(circle, rgba(13,6,73,1) 0%, rgba(85,42,214,1) 100%)",
+        "lightGradient": "radial-gradient(circle, rgba(204,204,204,1) 0%, rgba(224,224,224,1) 100%);"
       },
       colors:{
         NeonGreen: "#04f33d"
